1c442b81f44444a6be409610da529163baee9004
pablo <pabloiglesia@outlook.com>
2020-10-12 15:53:01 +0200

    ImageController.py

--- scripts/ImageController.py
+++ scripts/ImageController.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 
-import cv2, cv_bridge
+# import cv2, cv_bridge
 import os
+from PIL import Image
 
 class ImageController:
     def __init__(self, path=os.path.dirname(os.path.realpath(__file__))):
@@ -10,18 +11,18 @@ class ImageController:
         self.path = "{}/images".format(path)
 
     def record_image(self, msg):
-        # try:
-        img = self.to_cv2(msg)
-        cv2.imwrite('{}/img{:04d}.png'.format(self.path, self.indImage), img)
-        self.indImage += 1
-        #     return True
-        # except:
-        #     return False
+        # img = self.to_cv2(msg)
+        # cv2.imwrite('{}/img{:04d}.png'.format(self.path, self.indImage), img)
 
-    def to_cv2(self, msg, display=False):
-        img = self.bridge.imgmsg_to_cv2(msg)
-        if display:
-            cv2.namedWindow("window", 1)
-            cv2.imshow("window", self.image)
-            cv2.waitKey(5)
-        return img
+        size = (msg.width,msg.height)
+        img = Image.frombytes('RGB', size, msg.data)
+        img.save('{}/img{:04d}.png'.format(self.path, self.indImage))
+        self.indImage += 1
+
+    # def to_cv2(self, msg, display=False):
+    #     img = self.bridge.imgmsg_to_cv2(msg)
+    #     if display:
+    #         cv2.namedWindow("window", 1)
+    #         cv2.imshow("window", self.image)
+    #         cv2.waitKey(5)
+    #     return img
