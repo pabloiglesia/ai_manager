21be11950352c077a7041051d18890cf1d6108b7
PilarHB <pilarhbas@gmail.com>
2020-10-13 16:41:24 +0200

    New talker.py with service. Changes in CMakeLists.txt

--- scripts/talker.py
+++ scripts/talker.py
@@ -12,22 +12,24 @@ from std_msgs.msg import Float64MultiArray
 from ImageController import ImageController
 import random
 
+#from __future__ import print_function
+from AIManager.srv import GetActions, GetActionsResponse
 
-def gather_state_info(img_controller):
+# Global Image Controller
+IMAGE_CONTROLLER = ImageController()
+
+def gather_state_info():
     """
     This method gather information about the ur3 robot state by reading several ROS topics
     :param img_controller: class which will allow us to save sensor_msgs images
     """
 
-    current_coordinates = rospy.wait_for_message('/tasks/done', Float64MultiArray)  # To know if the previous task
+    # current_coordinates = rospy.wait_for_message('/tasks/done', Float64MultiArray)  # To know if the previous task
     # has been completed and get the current coordinates of the robot
 
     msg = rospy.wait_for_message('/usb_cam/image_raw', Image)  # We retrieve state image
-    img_controller.record_image(msg)  # We save the image in the replay memory
+    IMAGE_CONTROLLER.record_image(msg)  # We save the image in the replay memory
     # TODO: Gather information about the new state
-
-    return current_coordinates.data
-
 
 def rl_algorithm(current_coordinates):
     """
@@ -47,30 +49,43 @@ def rl_algorithm(current_coordinates):
         return actions[idx]
 
 
-def talker(publisher, action):
-    """
-    Publish the selected action into the ROS topic
-    :param action:
-    :type publisher: object
-    """
-    rospy.loginfo(action)  # Log in console
-    publisher.publish(action)  # Publish action
+# def talker(publisher, action):
+#     """
+#     Publish the selected action into the ROS topic
+#     :param action:
+#     :type publisher: object
+#     """
+#     rospy.loginfo(action)  # Log in console
+#     publisher.publish(action)  # Publish action
 
 
 def main():
-    publisher = rospy.Publisher('/tasks/action', String, queue_size=10)  # Publisher definition
+    # publisher = rospy.Publisher('/tasks/action', String, queue_size=10)  # Publisher definition
     rospy.init_node('ai_manager', anonymous=True)  # ROS node initialization
-    image = ImageController(capacity=10)  # Image controller initialization
+    get_actions_server()
+    # image = ImageController(capacity=10)  # Image controller initialization
 
-    while True:
-        current_coordinates = gather_state_info(image)  # Gathers state information
-        action = rl_algorithm(current_coordinates)  # Select action and train the algorithm
-        talker(publisher, action)  # Publish the action
+    # while True:
 
+        # action = rl_algorithm(current_coordinates)  # Select action and train the algorithm
+        # talker(publisher, action)  # Publish the action
+
+def handle_get_actions(req):
+    print("Returning action for coordinates" %(req.x, req.y))
+    current_coordinates = [req.a,req.b]
+    gather_state_info()  # Gathers state information
+    action = rl_algorithm(current_coordinates)
+    return GetActionsResponse(action)
+
+def get_actions_server():
+    #rospy.init_node('get_actions_server')
+    s = rospy.Service('get_actions', GetActions, handle_get_actions)
+    print("Ready to send actions.")
+    rospy.spin()
 
 if __name__ == '__main__':
     try:
-        print("Funciona")
         main()
+
     except rospy.ROSInterruptException:
         pass
